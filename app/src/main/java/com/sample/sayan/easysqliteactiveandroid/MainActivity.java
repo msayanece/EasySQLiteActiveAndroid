d8b8db2a180744b1bcd4d5a2ccbddca78ce664f5
Admin <anshu.p@encoders.co.in>
2018-01-12 18:09:36 +0530

    init commit

---
+++ app/src/main/java/com/sample/sayan/easysqliteactiveandroid/MainActivity.java
@@ -0,0 +1,48 @@
+package com.sample.sayan.easysqliteactiveandroid;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.activeandroid.ActiveAndroid;
+import com.sample.sayan.easysqliteactiveandroid.models.Student;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "sayanDB";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Student.deleteAllRows();
+//        Student.deleteStudentByRoll("A11");
+//        saveDataToDB();
+//        Student.updateStudentNameByRoll("A2", "Amit");
+        List<Student> students = Student.getAllStudents();
+        Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+    }
+
+    private void saveDataToDB() {
+        ActiveAndroid.beginTransaction();
+        try {
+            for (int i = 0; i < 20; i++) {
+                Student student = new Student();
+                student.name = "Sayan" + i;
+                student.roll = "A" + i;
+                student.className = "class" + i;
+                student.sectionName = "section" + i;
+                student.phone = "9863252141" + i;
+                student.save();
+            }
+            ActiveAndroid.setTransactionSuccessful();
+        }
+        finally {
+            ActiveAndroid.endTransaction();
+        }
+    }
+}
---
+++ app/src/main/java/com/sample/sayan/easysqliteactiveandroid/models/Student.java
@@ -0,0 +1,67 @@
+package com.sample.sayan.easysqliteactiveandroid.models;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Delete;
+import com.activeandroid.query.Select;
+import com.activeandroid.query.Update;
+
+import java.util.List;
+
+/**
+ * Created by Admin on 12-01-2018.
+ */
+
+@Table(name = "Student")
+public class Student extends Model {
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "roll")
+    public String roll;
+
+    @Column(name = "className")
+    public String className;
+
+    @Column(name = "sectionName")
+    public String sectionName;
+
+    @Column(name = "phone")
+    public String phone;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, String roll, String className, String sectionName, String phone) {
+        super();
+        this.name = name;
+        this.roll = roll;
+        this.className = className;
+        this.sectionName = sectionName;
+        this.phone = phone;
+    }
+
+    public static List<Student> getAllStudents() {
+        return new Select().from(Student.class).orderBy("roll DESC").execute();
+    }
+
+    public static Student getStudentByRoll(String roll) {
+        return new Select().from(Student.class).where("roll = ?", roll).executeSingle();
+    }
+
+    public static void updateStudentNameByRoll(String roll, String newName){
+        new Update(Student.class).set("name = ?",newName).where("roll = ?", roll).execute();
+    }
+
+    public static void deleteAllRows(){
+        ActiveAndroid.execSQL("DELETE FROM Student;");
+    }
+
+    public static void deleteStudentByRoll(String roll){
+        new Delete().from(Student.class).where("roll = ?", roll).execute();
+    }
+}
